838a32e56d1646672b4199c16716ff7467b8ea33
Etienne Tourigny <etienne.tourigny@bsc.es>
2017-03-30 12:01:33 +0200

    add timeseries from Jost's ecearth3 post utilities on CCA

---
+++ timeseries/timeseries.sh
@@ -0,0 +1,47 @@
+#/bin/ksh
+
+if [ "$#" -lt 1 ]; then
+   echo "Usage: timeseries.sh exp [user]"
+   echo "Compute timeseries for experiment exp of user (optional)"
+   exit
+fi
+
+exp=$1
+
+. $HOME/ecearth3/post/conf/conf_users.sh
+. $CONFDIR/conf_timeseries_$MACHINE.sh
+
+if [ "$#" -eq 2 ]; then
+   USERexp=$2
+fi
+
+conf=$MACHINE
+PROGDIR=$EMOP_DIR
+
+#Atmospheric timeseries
+
+cd $PROGDIR
+./monitor_atmo.sh -C $conf -R $exp -o
+./monitor_atmo.sh -C $conf -R $exp -e
+
+#Oceanic timeseries
+
+check=$( ls $POST_DIR/mon/Post_*/*sosaline* )
+
+if [ -z $check ] ; then
+echo "Atm-only!" 
+else
+./monitor_ocean.sh -C $conf -R $exp
+./monitor_ocean.sh -C $conf -R $exp -e
+fi
+
+# remember to add -f flag to force
+
+# create a tar and prepare the copy
+#DIR_TIME_SERIES=/perm/ms/it/ccjh/ecearth3/diag/timeseries
+cd ${DIR_TIME_SERIES}
+rm -r -f  timeseries_$exp.tar
+tar cfv timeseries_$exp.tar  $exp/
+ectrans -remote sansone -source timeseries_$exp.tar  -put -verbose -overwrite
+ectrans -remote sansone -source ~/EXPERIMENTS.$MACHINE.$USERme.dat -verbose -overwrite
+
