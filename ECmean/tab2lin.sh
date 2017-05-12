b515cbcc69d512f53cf274b9f656d207eac91bd6
Etienne Tourigny <etienne.tourigny@bsc.es>
2017-05-12 10:52:19 +0200

    make ECMean table formatting better

--- ECmean/tab2lin.sh
+++ ECmean/tab2lin.sh
@@ -15,20 +15,20 @@ sed -n "${row},${row}p" $f | cut -f 3
 exp=$1
 y1=$2
 y2=$3
-echo -n exp_y1_y2 "|"
+echo -n "|" exp_y1_y2 "|"
 f=Global_Mean_Table_${exp}_${y1}_${y2}.txt
 
-echo "TOAnet SW | TOAnet LW | Net TOA | Sfc Net SW | Sfc Net LW | SH Fl. | LH Fl. | SWCF | LWCF | NetSfc* | TOA-SFC | t2m | TCC | LCC | MCC | HCC | TP | P-E"
+echo " TOAnet SW | TOAnet LW | Net TOA | Sfc Net SW | Sfc Net LW | SH Fl. | LH Fl. | SWCF | LWCF | NetSfc* | TOA-SFC | t2m | TCC | LCC | MCC | HCC | TP | P-E |"
 
-echo -n ${exp}_${y1}_${y2} "|"
+echo -n "|" ${exp}_${y1}_${y2} "| "
 for row in 1 2 3 4 5 6 7 8 9 11 13
 do
-echo -n $(getcell $f $row )  "|"
+echo -n $(getcell $f $row )  "| "
 done
 
 for row in 1 3 4 5 6 7 11
 do
-echo -n $(getcell2 $f $row )  "|"
+echo -n $(getcell2 $f $row )  "| "
 done
 echo
 
